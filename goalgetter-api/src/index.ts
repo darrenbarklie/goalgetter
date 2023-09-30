c3846f593cbafc5cca38164f1f82fe3379717a7c
Darren Barklie <darrenbarklie@gmail.com>
2023-09-30 02:29:07 +0100

    Integrate Drizzle, connect to Turso hosted db, fetch data

--- goalgetter-api/src/index.ts
+++ goalgetter-api/src/index.ts
@@ -1,17 +1,26 @@
 import { Context, Elysia } from "elysia";
 import { swagger } from '@elysiajs/swagger'
+import { drizzle } from 'drizzle-orm/libsql';
+import { createClient } from '@libsql/client';
 
-import { getUserById } from "./user/index";
+import { getAllUsers, getUserById } from "./user/index";
 import { getGoalById } from "./goal/index";
 
 const app = new Elysia();
 
+// Database
+const client = createClient({ 
+    url: Bun.env.DATABASE_URL as string,
+    authToken: Bun.env.DATABASE_AUTH_TOKEN
+});
+export const db = drizzle(client);
+
 // Meta
 app.get("/", () => "GoalGetter API");
-app.get("/ping", () => "pong");
 app.get("/version", () => "v0.0.0");
 
 // Users
+app.get("/user", () => getAllUsers());
 app.get("/user/:id", ({ params: { id } }: {params: { id: number} }) => getUserById(id))
 
 // Goals
--- goalgetter-api/src/user/index.ts
+++ goalgetter-api/src/user/index.ts
@@ -1,3 +1,11 @@
+import { db } from "../index";
+import { User, users } from "../db/users";
+
+export const getAllUsers = async () => {
+  const result: User[] = await db.select().from(users).all();
+  return result;
+}
+
 export const getUserById = (id: number) => {
   return `Fetched user with id: ${id}`
 }
